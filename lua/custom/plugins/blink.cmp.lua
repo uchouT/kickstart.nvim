ad7f1cd36d265379df17ab1b19fcb9d967b3d73e
uchouT <xtx40@outlook.com>
2025-04-18 18:45:05 +0800

    custom my config

---
+++ lua/custom/plugins/blink.cmp.lua
@@ -0,0 +1,12 @@
+return
+{
+    'saghen/blink.cmp',
+    opts = {
+      keymap = {
+        preset = 'enter',
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+      },
+
+    },
+  }
